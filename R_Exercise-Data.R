e57278f96c6ddcbc20c549b9f148f9008059dba6
hyginn <boris.steipe@utoronto.ca>
2016-02-07 17:38:19 -0500

    Init

--- R_Exercise-Data.R
+++ R_Exercise-Data.R
@@ -10,8 +10,10 @@
 "Before we work with data, we obviously need to read data into our R session. That is, unless we create (random?) data from first principles. This set of exercises focusses on how to read and write data in various formats and from various sources.
 
 What you should already know includes:
-- data types in R;
+- data types in R (logical, character, numeric...);
 - vectors, matrices, data frames and lists, and the difference between them;
+- creating vectors with the c() function and using the related rbind() and
+    cbind() functions for matrices or dataframes;
 
 "
 
@@ -19,7 +21,14 @@ What you should already know includes:
 # ====================================================================
 #        PART ONE: READING AND WRITING LOCAL TEXT DATA
 # ====================================================================
+'The most simple-minded way to get data into R is of course to define it in a script file and assign it. I find myself from time to time putting words or numbers into a text file and replacing all linebreaks with ", " ; wrap this into the c() function, copy it into a script, assign it and you are done.'
 
+"Sometimes this is the right way to do, in particular if the data isn't well structured and needs to be edited in a text file anyway. But more frequently our data has some predictable structure and usually it looks this way:
+
+
+
+
+"
 
 # ===== Subtopic =====================================================
 
