db4ed6441be2808103dcb1e2f35a5f9dd570e36c
hyginn <boris.steipe@utoronto.ca>
2016-02-07 17:21:36 -0500

    Init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# R_Exercise-Data
+# R_Exercises
+## Data
+
+Before we work with data, we obviously need to read data into our R session. This set of exercises focusses on how to read and write data in various formats and from various sources.
+
+B. Steipe (Feb 2016)
---
+++ R_Exercise-Data.R
@@ -0,0 +1,68 @@
+# R_exercise-subsetting.R
+#
+# Practising filtering of data via R's subsetting methods
+#
+# Boris Steipe (boris.steipe@utoronto.ca)
+# Jan 2016
+# (C) copyrighted material. Do not share without permission.
+# ====================================================================
+
+"Before we work with data, we obviously need to read data into our R session. That is, unless we create (random?) data from first principles. This set of exercises focusses on how to read and write data in various formats and from various sources.
+
+What you should already know includes:
+- data types in R;
+- vectors, matrices, data frames and lists, and the difference between them;
+
+"
+
+
+# ====================================================================
+#        PART ONE: READING AND WRITING LOCAL TEXT DATA
+# ====================================================================
+
+
+# ===== Subtopic =====================================================
+
+
+
+
+
+# ====================================================================
+#        PART TWO: BINARY DATA: THE .RDATA FORMAT
+# ====================================================================
+
+
+# ===== Subtopic =====================================================
+
+
+
+# ====================================================================
+#        PART THREE: DATA FROM THE WEB
+# ====================================================================
+
+
+# ===== Subtopic =====================================================
+
+
+
+
+
+
+
+
+
+# ====================================================================
+#        APPENDIX: OUTLOOK
+# ====================================================================
+"There are many more function for data preparation that this tutorial did not cover. You should know about the following functions:"
+
+...
+
+"And you should know about the following packages:
+
+...
+"
+
+
+
+# [END]
